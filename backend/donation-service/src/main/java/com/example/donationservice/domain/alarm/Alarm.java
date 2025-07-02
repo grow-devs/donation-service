93c80bd0dcf0e36eeff2517e69b071e92586e933
wjdgur778 <wjdgur778@gmail.com>
2025-07-02 16:58:54 +0900

    Revert "delete backend package"
    This reverts commit 32cf2fe525dba943b91d0c022bae6c072ba75809.

---
+++ backend/donation-service/src/main/java/com/example/donationservice/domain/alarm/Alarm.java
@@ -0,0 +1,26 @@
+package com.example.donationservice.domain.alarm;
+
+import com.example.donationservice.domain.user.User;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Alarm {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private AlarmType type;
+
+    private String message;
+
+    private Long postId; // 게시글로 이동하기 위해서
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+}
