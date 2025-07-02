22879cfeb39d40aa7479bb86b0af69c059df7ecc
wjdgur778 <wjdgur778@gmail.com>
2025-07-02 15:30:02 +0900

    delete backend package
    # Conflicts:
    #	backend/donation-service/build.gradle
    #	backend/donation-service/src/main/java/com/example/donationservice/DonationServiceApplication.java
    #	backend/donation-service/src/main/java/com/example/donationservice/domain/user/User.java

--- backend/donation-service/src/main/java/com/example/donationservice/domain/alarm/AlarmType.java
+++
@@ -1,8 +0,0 @@
-package com.example.donationservice.domain.alarm;
-
-public enum AlarmType {
-    DONATION,
-    COMMENT,
-    GOAL_REACHED,
-    DEADLINE_REACHED,
-}
--- backend/donation-service/src/main/java/com/example/donationservice/domain/user/ApprovalStatus.java
+++
@@ -1,7 +0,0 @@
-package com.example.donationservice.domain.user;
-
-public enum ApprovalStatus {
-    PENDING,
-    ACCEPTED,
-    REJECTED,
-}
