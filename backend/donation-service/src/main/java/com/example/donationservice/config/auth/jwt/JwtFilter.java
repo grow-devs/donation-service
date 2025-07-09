520308973414b06f265108a19bf0644dd06d0d61
wjdgur778 <wjdgur778@gmail.com>
2025-07-09 11:41:32 +0900

    [feat] jwtfilter 수정 -> access토큰이 만료되면 refresh토큰을 통한 access토큰 재발급

--- backend/donation-service/src/main/java/com/example/donationservice/config/auth/jwt/JwtFilter.java
+++ backend/donation-service/src/main/java/com/example/donationservice/config/auth/jwt/JwtFilter.java
@@ -1,12 +1,17 @@
 package com.example.donationservice.config.auth.jwt;
 
 import com.example.donationservice.domain.user.CustomUserDetailService;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
@@ -21,12 +26,12 @@ public class JwtFilter extends OncePerRequestFilter {
 
     private final JwtService jwtService;
     private final CustomUserDetailService customUserDetailService;
-
+    private final RedisTemplate<String, Object> redisTemplate;
     @Override
     protected void doFilterInternal(
             HttpServletRequest request,
-            HttpServletResponse response,
-            FilterChain filterChain
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain
     ) throws ServletException, IOException {
         final String authorizationHeader = request.getHeader("Authorization");
         final String jwt;
@@ -46,20 +51,47 @@ public class JwtFilter extends OncePerRequestFilter {
         // loadUserByUsername을 통해 contextholder에 그 정보를 저장하고 있지 않다면,
         if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
             UserDetails userDetails = customUserDetailService.loadUserByUsername(userEmail);
-            if (jwtService.isTokenValid(jwt, userDetails)) {
-                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
-                        userDetails,
-                        null,
-                        userDetails.getAuthorities()
-                );
-                authToken.setDetails(
-                        new WebAuthenticationDetailsSource().buildDetails(request)
-                );
-                // securiyContextHolder에 저장
-                SecurityContextHolder.getContext().setAuthentication(authToken);
+
+            try{
+                //jwt가 유효하면 security contextholder에 사용자를 저장한다.
+                if(jwtService.isTokenValid(jwt,userDetails)){
+                    String username = jwtService.getUserNameFromJwtToken(jwt);
+                    setAuthentication(userDetails);
+                }
+            }
+            catch (ExpiredJwtException e) {
+                //Access Token이 만료된 경우 → Refresh Token 검증 로직 실행
+                handleExpiredAccessToken(request, response, userDetails);
+            } catch (JwtException e) {
+                // todo 위조된 토큰 → 401 에러 응답 글로벌 에러 필요
+                throw new RuntimeException();
             }
         }
         //jwtfilter를 거친 후 다음 필터로 향하게 한다. (UsernamePasswordAuthenticationfilter)
         filterChain.doFilter(request,response);
+    }
+
+    private void setAuthentication(UserDetails userDetails) {
+        Authentication authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+    }
+
+    private void handleExpiredAccessToken(HttpServletRequest request, HttpServletResponse response, UserDetails userDetails){
+        String username = userDetails.getUsername();
+        String refreshToken = (String) redisTemplate.opsForValue().get(username); // Redis에서 Refresh Token 가져오기
+
+        //등록된 refreshToken이 존재한다면 accesstoken을 재발급해야한다.
+        if(refreshToken!=null){
+            String newAccessToken = jwtService.generateToken(username);
+            setAuthentication(userDetails);
+            response.setHeader("Authorization", "Bearer " + newAccessToken);
+        }
+        //등록된 refreshToken이 없다면
+        else{
+            System.out.println("there is no refresh token");
+
+            //TODO globalException 필요
+            throw new RuntimeException();
+        }
     }
 }
