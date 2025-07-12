25a7ad38bbaf519ad027cf4df066ce47afb2bdd8
wjdgur778 <wjdgur778@gmail.com>
2025-07-13 00:38:00 +0900

    [feat] authStore -> zuztand를 통한 로그인상태 및 권한 상태관리. isLoggedIn,userRole,nickName 추가

--- frontend/donation-service/src/store/authStore.js
+++ frontend/donation-service/src/store/authStore.js
@@ -3,8 +3,11 @@ import {create} from 'zustand';
 const useAuthStore = create((set) => ({
     isLoggedIn: false,
     userRole: null,
-    setLoggedIn: (value) => set({ isLoggedIn: value }),
+    nickName: null,
+    login: () => set({ isLoggedIn: true }),
+    logout: () => set({ isLoggedIn: false }),
     setUserRole: (role) => set({ userRole: role }),
+    setNickName: (nickname) => set({ nickName: nickname })
 }))
 
 export default useAuthStore;
