b8451404a349a778eb4591af6d216236a98b4e6f
wjdgur778 <wjdgur778@gmail.com>
2025-07-15 12:58:39 +0900

    [feat] querydsl을 위한 PostRepositoryCustom 상속

--- backend/donation-service/src/main/java/com/example/donationservice/domain/post/PostRepository.java
+++ backend/donation-service/src/main/java/com/example/donationservice/domain/post/PostRepository.java
@@ -1,6 +1,7 @@
 package com.example.donationservice.domain.post;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PostRepository extends JpaRepository<Post,Long> {
+// Querydsl이 제공하는 QuerydslPredicateExecutor를 상속받아 더 유연한 쿼리도 가능하지만,
+// 현재는 PostRepositoryCustom을 상속받아 커스텀 메서드를 사용합니다.
+public interface PostRepository extends JpaRepository<Post,Long>,PostRepositoryCustom {
 }
