29c89ded3e00d895ab6bc5c83fb80bd0bdc10046
wjdgur778 <wjdgur778@gmail.com>
2025-07-15 12:51:41 +0900

    [feat] Post엔티티에 participants추가 -> Donation 발생 시 트랜잭션 내에서 이 필드를 업데이트하는 카운터 캐싱을 한다.

--- backend/donation-service/src/main/java/com/example/donationservice/domain/post/Post.java
+++ backend/donation-service/src/main/java/com/example/donationservice/domain/post/Post.java
@@ -36,6 +36,8 @@ public class Post extends BaseTimeEntity {
 
     private ApprovalStatus approvalStatus;
 
+    private Long participants;//Donation 발생 시 트랜잭션 내에서 이 필드를 업데이트하는 카운터 캐싱을 한다.
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "team_id")
     private Team team;
@@ -43,5 +45,6 @@ public class Post extends BaseTimeEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "category_id")
     private Category category;
+
 
 }
