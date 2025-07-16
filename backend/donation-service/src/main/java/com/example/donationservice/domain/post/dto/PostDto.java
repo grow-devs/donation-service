13e97a429e465eb6d89b040211817da10db598fc
wjdgur778 <wjdgur778@gmail.com>
2025-07-17 02:21:40 +0900

    [feat] imageFile 필드 추가및 필요없는 teamId와 approvalStatus 필드 삭제

--- backend/donation-service/src/main/java/com/example/donationservice/domain/post/dto/PostDto.java
+++ backend/donation-service/src/main/java/com/example/donationservice/domain/post/dto/PostDto.java
@@ -2,16 +2,17 @@ package com.example.donationservice.domain.post.dto;
 
 import com.example.donationservice.domain.post.Post;
 import com.example.donationservice.domain.user.ApprovalStatus;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class PostDto {
 
     @Getter
+    @Setter//todo 개시물 생성시 데이터 바인딩 에러를 해결해보기 위해 setter 사용
     @Builder
     @NoArgsConstructor
     @AllArgsConstructor
@@ -19,13 +20,15 @@ public class PostDto {
         private String title;
         private String content;
         private Long targetAmount;
+        @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
         private LocalDateTime deadline;
-        private String imageUrl;
-        private Long teamId;         // team, category는 엔티티가므로 id만 받음
+//        private Long teamId;         // team, category는 엔티티가므로 id만 받음
         private Long categoryId;
-        private ApprovalStatus approvalStatus; // 생성 시 기본값이라면 여기서 안 받아도 됨
-        private Long currentAmount; //필터링을 테스트하기 위해 임시로 생성
-        private Long participants; //필터링을 테스트하기 위해 임시로 생성
+//        private ApprovalStatus approvalStatus; //todo 생성 시 기본값이라면 여기서 안 받아도 됨
+        private Long currentAmount; //todo 필터링을 테스트하기 위해 임시로 생성
+        private Long participants; //todo 필터링을 테스트하기 위해 임시로 생성
+
+        private MultipartFile imageFile;
     }
 
     @Getter
@@ -37,14 +40,26 @@ public class PostDto {
         private String title;
         private String content;
         private LocalDateTime createdAt; // 최신순 정렬을 위한 만들어진 시간 응답
+        private LocalDateTime updatedAt; // 최신순 정렬을 위한 만들어진 시간 응답
         private Long currentAmount;
         private Long targetAmount;
         private LocalDateTime deadline;
         private String imageUrl;
         private ApprovalStatus approvalStatus;
         private Long teamId;
+        private String teamName;
         private Long categoryId;
+        private String categoryName;
         private Long participants;
+    }
+
+    @Getter
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class PostResponseWithTotalCount {
+        private List<PostResponse> resultList;
+        private long totalCount;
     }
 
     public static PostResponse from(Post post) {
