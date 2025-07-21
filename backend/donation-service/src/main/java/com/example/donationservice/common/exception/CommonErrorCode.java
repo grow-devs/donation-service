8af086451678f87023c1c80ef7568f3d8ade4bb7
wjdgur778 <wjdgur778@gmail.com>
2025-07-21 18:25:54 +0900

    [chore] 이미지 업로드 실패 코드 추가

--- backend/donation-service/src/main/java/com/example/donationservice/common/exception/CommonErrorCode.java
+++ backend/donation-service/src/main/java/com/example/donationservice/common/exception/CommonErrorCode.java
@@ -9,7 +9,8 @@ public enum CommonErrorCode implements ErrorCode{
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "유저를 조회할 수 없습니다."),
     TEAM_ALREADY_EXISTS(HttpStatus.BAD_REQUEST, "이미 후원 단체가 존재합니다."),
     // 로그인 인증 실패 에러
-    LOGIN_FAILED(HttpStatus.UNAUTHORIZED, "로그인에 실패했습니다. 아이디와 비밀번호를 확인해주세요.");
+    LOGIN_FAILED(HttpStatus.UNAUTHORIZED, "로그인에 실패했습니다. 아이디와 비밀번호를 확인해주세요."),
+    FAIL_UPLOAD_IMAGE(HttpStatus.INTERNAL_SERVER_ERROR, "이미지 업로드에 실패하였습니다."); // 이미지 업로드 에러 추가
 
     private final HttpStatus status;
     private final String message;
