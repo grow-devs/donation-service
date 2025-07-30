fc00a0de23a5edbec91a99e38c0f0fda84128bfa
wjdgur778 <wjdgur778@gmail.com>
2025-07-31 01:23:39 +0900

    [feat] UpdateReadByAlarmId -> 알림 읽기 시에 직접 update 쿼리로 isRead를 수정

--- backend/donation-service/src/main/java/com/example/donationservice/domain/alarm/AlarmRepository.java
+++ backend/donation-service/src/main/java/com/example/donationservice/domain/alarm/AlarmRepository.java
@@ -1,7 +1,19 @@
 package com.example.donationservice.domain.alarm;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface AlarmRepository extends JpaRepository<Alarm, Long> {
+    Page<Alarm> findByUserId(Long userId, Pageable pageable);
 
+    @Modifying
+    @Query("update Alarm a SET a.isRead = true WHERE a.id = :alarmId")
+    void UpdateReadByAlarmId(Long alarmId);
+
+    int countByUserIdAndIsReadFalse(Long userId);
 }
