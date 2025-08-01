7afe74fd6d3d7b6b63e6337cde93f6eb5f093202
JeongTaekgyu <taekgyu0602@gmail.com>
2025-08-01 22:12:23 +0900

    [chore] 사용하지 않는 test api 제거

--- backend/donation-service/src/main/java/com/example/donationservice/domain/user/UserController.java
+++ backend/donation-service/src/main/java/com/example/donationservice/domain/user/UserController.java
@@ -10,7 +10,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
@@ -80,44 +79,6 @@ public class UserController {
                 Result.builder()
                         .message("유저 좋아요 게시글 조회 성공")
                         .data(userPostLikeInfo)
-                        .build()
-        );
-    }
-
-
-    /**
-     * TODO. 파라미터로 Authentication 대신 @AuthenticationPrincipal CustomUserDetail을 통해 인증된 객체를 바로 주입받는다.
-     *       이를 통해 user의 정보를 간단하게 가져올 수 있다. (ex. userId)
-     *
-     * @param customUserDetail
-     * @return
-     */
-    @PostMapping("/test")
-    public ResponseEntity<Result> test(@AuthenticationPrincipal CustomUserDetail customUserDetail){
-        System.out.println(customUserDetail.getUserId());
-        System.out.println("---------------------------------------------");
-
-        return ResponseEntity.status(200).body(
-                Result.builder()
-                        .message("로그인 성공")
-                        .data(null)
-                        .build()
-        );
-    }
-
-    @GetMapping("/test")
-    public ResponseEntity<Result> testAccessToken(Authentication authentication) {
-        System.out.println("~~~~~~~ AccessToken 인증 테스트 ~~~~~~~~~");
-        // 토큰 출력
-        System.out.println("Authentication: " + authentication);
-
-        // 인증이 성공하면 여기까지 들어오고,
-        // Authentication 객체를 통해 유저 정보 확인 가능
-        String username = authentication.getName(); // 이메일 (principal)
-        return ResponseEntity.ok(
-                Result.builder()
-                        .message("AccessToken 인증 성공")
-                        .data("로그인된 사용자: " + username)
                         .build()
         );
     }
