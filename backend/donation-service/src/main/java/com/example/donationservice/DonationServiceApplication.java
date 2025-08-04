3277c22004a15e91f616d4810150e6f472350244
wjdgur778 <wjdgur778@gmail.com>
2025-08-05 00:06:03 +0900

    [feat] 스케줄링 활성화

--- backend/donation-service/src/main/java/com/example/donationservice/DonationServiceApplication.java
+++ backend/donation-service/src/main/java/com/example/donationservice/DonationServiceApplication.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
 @EnableJpaAuditing // 엔티티의 생성/수정 시점과 주체를 자동으로 추적하는 JPA Auditing 기능을 활성화
 @EnableAsync // 비동기 메소드 실행을 활성화
+@EnableScheduling // 스케줄링 활성화
 public class DonationServiceApplication {
 
     public static void main(String[] args) {
