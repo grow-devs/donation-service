acbb50929b292153010d335ae27386dff980a08e
JeongTaekgyu <taekgyu0602@gmail.com>
2025-09-01 17:36:00 +0900

    [feat] 임시 스케줄러 및 임시 삭제

--- backend/donation-service/src/main/java/com/example/donationservice/scheduler/MetaDataScheduler.java
+++ backend/donation-service/src/main/java/com/example/donationservice/scheduler/MetaDataScheduler.java
@@ -21,8 +21,8 @@ public class MetaDataScheduler {
     private final RedisTemplate<String,String> redisTemplate;
 
     // 총 모금액 계산 스케줄러
-    @Scheduled(cron = "0 47 17 * * ?")  // 매일 2AM
-//    @Scheduled(fixedRate = 5000)  // 테스트를 위한 5초 스케줄
+//    @Scheduled(cron = "0 47 17 * * ?")  // 매일 2AM
+    @Scheduled(fixedRate = 600000)  // 테스트를 위한 5초 스케줄
     @Transactional
     public void updateTotalDonationAmount(){
         Long totalAmount = postRepository.sumAllDonationAmounts();//post에서 sum함으로써 계산량을 줄인다.
@@ -40,8 +40,8 @@ public class MetaDataScheduler {
 //    ########## 총 후원자수 저장 스캐줄러 ########
 //    일일 통계처럼 매일 갱신되는 데이터의 경우,
 //    Redis에 이미 저장된 키에 새로운 값으로 덮어쓰는 것이 가장 효율적인 방법
-    @Scheduled(cron = "0 47 17 * * ?")  // 매일 00시 //todo 오늘에 관한 정보이다보니 언제 스케줄링을 돌릴지도 생각해야함
-//    @Scheduled(fixedRate = 5000)  // 테스트를 위한 5초 스케줄
+//    @Scheduled(cron = "0 47 17 * * ?")  // 매일 00시 //todo 오늘에 관한 정보이다보니 언제 스케줄링을 돌릴지도 생각해야함
+    @Scheduled(fixedRate = 600000)  // 테스트를 위한 5초 스케줄
     @Transactional
     public void updateTotalDonors(){
         Long totalDonors = donationRepository.CountDistinctDonors();
@@ -51,14 +51,15 @@ public class MetaDataScheduler {
 
 //    ########## 오늘의 첫 기부 데이터 key 삭제 스케줄러 ########
     // 키를 삭제하지 않으면 어제 혹은 그 이전에 기부했던 정보가 그대로 있게된다.
-    @Scheduled(cron = "0 47 17 * * ?")  // 매일 23시 59분 //todo 오늘에 관한 정보이다보니 언제 스케줄링을 돌릴지도 생각해야함
-//    @Scheduled(fixedRate = 600000)  // 테스트를 위한 5초 스케줄
+//    @Scheduled(cron = "0 47 17 * * ?")  // 매일 23시 59분 //todo 오늘에 관한 정보이다보니 언제 스케줄링을 돌릴지도 생각해야함
+    @Scheduled(fixedRate = 600000)  // 테스트를 위한 5초 스케줄
     @Transactional
     public void DeleteFirstDonationKey(){
         String key = "first_donation:";
-        redisTemplate.opsForHash().delete(key,"nickName");
-        redisTemplate.opsForHash().delete(key,"createdAt");
+//        redisTemplate.opsForHash().delete(key,"nickName");
+//        redisTemplate.opsForHash().delete(key,"createdAt");
 
+        redisTemplate.delete(key);
         redisTemplate.delete("ranking:daily:20250901");
 
         System.out.println("redis의 오늘의 첫 기부데이터 key = first_donation: 삭제를 완료했습니다.");
--- backend/donation-service/src/main/java/com/example/donationservice/scheduler/PostScheduler.java
+++ backend/donation-service/src/main/java/com/example/donationservice/scheduler/PostScheduler.java
@@ -12,7 +12,8 @@ public class PostScheduler {
     private final PostService postService;
 
     // 데드라인이 지난 게시물에게 알림을 보내는 스케줄러
-    @Scheduled(cron = "0 47 17 * * *") //
+    @Scheduled(cron = "0 0 0 * * *") //
+//    @Scheduled(fixedRate = 600000)  // 테스트를 위한 10분 스케줄
     public void sendDeadlinePassedNotifications() {
         postService.sendDeadlinePassedNotifications();
     }
